d6adb8c8c95bd2b39ae1a8306142a46242028abb
cnickerson24 <144709136+cnickerson24@users.noreply.github.com>
2024-01-29 09:29:35 -0500

    updated Sketch.js

--- sketch.js
+++ sketch.js
@@ -16,9 +16,8 @@ function draw() {//ruins in a loop
   var num = 2;
   var sidelen = windowWidth/num;
   
-    for (var y = 0; y < windowHeight; y= y +sidelen){
-    for (var x = 0; x < windowWidth; x= x +sidelen){
-    
+    for (var y = 0; y < 2* windowHeight; y= y +sidelen){
+    for (var x = 0; x < 2* windowWidth; x= x +sidelen){    
     image (gif,x,y,sidelen,sidelen);
 
 function windowResized(){
