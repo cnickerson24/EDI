4ed57d46ba8e4dc710c205f26925dca00ad02ac5
cnickerson24 <144709136+cnickerson24@users.noreply.github.com>
2024-01-30 10:38:37 -0500

    update

--- CreativeCoding/finalBG/sketch.js
+++ CreativeCoding/finalBG/sketch.js
@@ -1,7 +1,7 @@
 let gif;
 
 function preload (){
-gif=loadImage ('assets/redsquare800pi.gif');
+gif=loadImage ('assets/Final.gif');
 }
 
 
@@ -13,11 +13,12 @@ createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {//ruins in a loop
-  var num = 2;
+background ('yellow');
+  var num = 8;
   var sidelen = windowWidth/num;
   
-    for (var y = 0; y < windowHeight; y= y +sidelen){
-    for (var x = 0; x < windowWidth; x= x +sidelen){
+    for (var y = 0; y < 2 * windowHeight; y= y +sidelen){
+    for (var x = 0; x < 2 * windowWidth; x= x +sidelen){
     
     image (gif,x,y,sidelen,sidelen);
 
